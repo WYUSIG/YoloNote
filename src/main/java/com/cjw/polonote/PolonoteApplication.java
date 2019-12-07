5672ae4e87c0f93a83520ce312d6150672b595f2
WYUSIG <13169103974@163.com>
2019-12-07 08:35:12 +0800

    第一次提交

---
+++ src/main/java/com/cjw/polonote/PolonoteApplication.java
@@ -0,0 +1,13 @@
+package com.cjw.polonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolonoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolonoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cjw/polonote/PolonoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjw.polonote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolonoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
