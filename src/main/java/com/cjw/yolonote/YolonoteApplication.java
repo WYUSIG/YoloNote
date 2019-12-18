615e79f7e1ee4f95882cc6948ec4d2756f5acb95
“WYUSIG <13169103974@163.com>
2019-12-18 18:04:59 +0800

    模板

--- src/main/java/com/cjw/yolonote/YolonoteApplication.java
+++ src/main/java/com/cjw/yolonote/YolonoteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PolonoteApplication {
+public class YolonoteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PolonoteApplication.class, args);
+        SpringApplication.run(YolonoteApplication.class, args);
     }
 
 }
